8b3d49ca459c8d2b98e622477660436339fb1fd6
Reza-Irvando <rezairvando@mail.ugm.ac.id>
2023-03-15 13:10:54 +0700

    [FIX] fund js & jankenpon js

--- javascript/fundamental.js
+++ javascript/fundamental.js
@@ -1,3 +1,4 @@
+// Variable JavaScript
 console.log("Hello World");
 var nama = "Reza Irvando";
 let id = 4765717;
@@ -11,6 +12,11 @@ parag.innerHTML = "Haloo, Saya " + nama + " , dengan ID " + id + ", alamat di " 
 body.append(judul);
 body.append(parag);
 
+//Pop Up
+alert("Selamat datang di web Reza Irvando \nWeb ini berisi tentang penerapan fundamental JavaScript");
+alert("Sekarang tanggal: " + new Date());
+
+// Tipe Data JavaScript
 let a = 12345;
 let b = 123.45;
 let c = "Arkatama";
@@ -24,8 +30,6 @@ let h = {
     id : 4765717,
     SIB : "Arkatama"
 }
-
-// Tipe Data JavaScript
 console.log("~~Tipe data~~");
 console.log(a, typeof a);
 console.log(b, typeof b);
@@ -35,13 +39,16 @@ console.log(e, typeof e);
 console.log(f, typeof f);
 console.log(g, typeof g);
 console.log(h, typeof h);
+let gvalue = String(g)
+alert("Tipe data JavaScript" + "\na = " + a + " , tipe data " + typeof a + "\nb = " + b + " , tipe data " + typeof b + "\nc = " + c +" , tipe data " + typeof c + "\nd = " + d +" , tipe data " + typeof d + "\ne = " + e +" , tipe data " + typeof e + "\nf = " + f +" , tipe data " + typeof f + "\ng = " + gvalue + " , tipe data " + typeof g + "\nh = " + h +" , tipe data " + typeof h);
 
 // Konversi Tipe Data
 console.log("~~Konversi Tipe Data~~")
-let value = false;
-value = String(value);
-console.log(value, typeof value);
+let value1 = false;
+value2 = String(value1);
+console.log(value2, typeof value2);
 console.log("6" / "2");
+let value3 = ("6" / "2");
 let str = "123";
 console.log("str bertipe ", typeof str, " dengan value : ", str);
 let num = Number(str);
@@ -50,18 +57,21 @@ console.log(Boolean(1));
 console.log(Boolean(0));
 console.log(Boolean("hello"));
 console.log(Boolean(""));
+alert("Konversi Tipe Data \nvalue1 = " + value1 + " bertipe " + typeof value1 + " menjadi value2 = " + value2 + " bertipe " + typeof value2 + "\n('6'/'2') menghasilkan nilai " + value3 +"\nstr = '123' bertipe " + typeof str + " menjadi bernilai " + num + " bertipe " + typeof num + "\nBoolean(1) dan Boolean('hello) sama-sama bernilai " + Boolean('hello') + " bertipe " + typeof Boolean('hello') + "\nBoolean(0) dan Boolean('') sama-sama bernilai " + Boolean('') + " bertipe " + typeof Boolean(''));
 
 // Operator Aritmatika
 console.log("~~Operator Aritmatika~~");
 let i = 20;
 let j = 5;
 console.log("i = 20 , j = 5")
-console.log("Penjumlahan : ",i + j);
-console.log("Pengurangan : ",i - j);
-console.log("Perkalian : ",i * j);
-console.log("Pembagian : ",i / j);
-console.log("Perpangkatan : ",i ** j);
-console.log("Sisa Bagi : ",i % j);
+console.log("Penjumlahan : " + i + j);
+console.log("Pengurangan : " + i - j);
+console.log("Perkalian : " + i * j);
+console.log("Pembagian : " + i / j);
+console.log("Perpangkatan : " + i ** j);
+console.log("Sisa Bagi : " + i % j);
+alert("Operator Aritmatika \ni = " + i + " dan j = " + j + "\ni + j = " + (i+j) + "\ni - j = " + (i-j) + "\ni * j = " + (i*j) + "\ni / j = " + (i/j) + "\ni ** j = " + (i**j) + "\ni % j = " + (i%j));
+
 
 // Operator Assignment
 console.log("~~Operator Assignment~~");
@@ -79,6 +89,9 @@ k **= 3;
 console.log(k);
 k %= 3;
 console.log(k);
+k = 2;
+alert("Operator Aritmatika \nk = " + k + "\nk += 5 hasilnya " + (k+=5) + "\nk -= 3 hasilnya " + (k-=3) + "\nk *= 2 hasilnya " + (k*=2) + "\nk /= 2 hasilnya " + (k/=2) + "\nk **= 3 = " + (k**=3) + "\nk %= 3 = " + (k%=3));
+
 
 // Operator Logika
 console.log("~~Operator Logika~~");
@@ -87,6 +100,7 @@ let m = false;
 console.log(l && m);
 console.log(l || m);
 console.log(!l);
+alert("Operator Logika \nl = " + l + " dan m = " + m + "\(nl && m) hasilnya " + (l&&m) + "\n(l || m) hasilnya " + (l||m) + "\n(!l) hasilnya " + (!l));
 
 //Operator Bitwise
 console.log("~~Operator Bitwise~~");
@@ -96,56 +110,14 @@ console.log(n  & o);
 console.log(n  | o);
 console.log(n  ^ o);
 console.log(~n);
+alert("Operator Bitwise \nn = " + n + " dan o = " + o + "\n(n & o) hasilnya " + (n&o) + "\n(n | o) hasilnya " + (n|o) + "\n(n ^ o) hasilnya " + (n^o) + "\n(~n) hasilnya " + (~n));
 
 //Operator Ternary
 console.log("~~Operator Ternary~~")
 let p = 1;
 let q = 1;
 console.log(p == q ? "yes" : "no");
-
-//Pop Up : Alert
-alert("Selamat datang di web Reza Irvando");
-alert("Sekarang tanggal: " + new Date());
-
-//Pop Up : Prompt
-let response1 = prompt("Apakah kamu mau nonton movie anime Suzume no Tojimari akhir pekan ini?");
-alert("Jawabannya adalah " + response1)
-
-//Pop Up : Confirm
-let response2 = confirm("Okee kamu yang bayar yaa Heheee");
-if (response = true){
-    alert("Asiiiik Gaaaasssss");
-}
-else {
-    alert("Yaaahhh, Padahal sebenernya aku mau bayarin :(")
-}
-
-//Conditional
-alert("Waktunya maen games!!!");
-let soal1 = prompt("Hasil 2 + 3 adalah ...");
-if(soal1 == 5){
-    alert("Waah kamu benar");
-}
-else{
-    alert("Yaah kamu salah");
-}
-let soal2 = prompt("Pilih salah satu angka 1 sampai 3!")
-switch (soal2){
-    case "1":
-        alert("Kamu wibuuu");
-        break;
-    case "2":
-        alert("Pacarmu Animeee");
-        break;
-    case "3":
-        alert("Kamu gapunya temen hehee");
-        break;
-    default:
-        alert("Salah input bwang!!")
-        break;
-}
-
-alert("Sesi Pop Up Berakhir. Silahkan Check Console!")
+alert("Operator Ternary \n p = " + p + " dan q = " + q + "\nuntuk (p == q ? 'yes' : 'no') hasilnya adalah " + (p==q ? "yes":"no"))
 
 //Loop JavaScript
 console.log("~~Loop JavaScript~~");
@@ -202,3 +174,56 @@ var salam4 = () => console.log("4. Hello World!");
 
 salam3();
 salam4();
+
+//Conditional
+let response1 = prompt("Pop Up Conditional \nApakah kamu mau nonton movie anime Suzume no Tojimari akhir pekan ini? (y/t)");
+if (response1 == "y"){
+    let response2 = confirm("Okee kamu yang bayar yaa Heheee");
+    if (response2 = true){
+        alert("Asiiiik Gaaaasssss");
+    }
+    else {
+        alert("Bercandaaa, gasss laah heeheee");
+    }
+}
+else if(response1 == "t"){
+    alert("Waduuuh, yaudah mungkin lain kali aja :(");
+}
+else{
+    alert("Input tidak valid");
+}
+
+alert("Waktunya maen games!!!");
+let soal1 = prompt("Hasil 2 + 3 adalah ...");
+if(soal1 == 5){
+    alert("Yeaaayy, jawaban kamu benar");
+}
+else if (soal1 == null){
+    alert("Input tidak valid")
+}
+else{
+    alert("Waduuuh, jawaban kamu salah");
+}
+
+let soal2 = prompt("Pilih salah satu angka 1 sampai 3!")
+switch (soal2){
+    case "1":
+        alert("Kamu wibuuu");
+        break;
+    case "2":
+        alert("Pacarmu Animeee");
+        break;
+    case "3":
+        alert("Kamu gapunya temen hehee");
+        break;
+    default:
+        alert("Salah input bwang!!")
+        break;
+}
+
+let ulangtext = prompt("Looping Javascript \nMasukkan jumlah perulangan alert Hello World!")
+for(let t=0; t<ulangtext; t++){
+    alert("Hello World! ke-" + (t+1))
+}
+
+alert("Sesi Pop Up Berakhir. Silahkan Check Console!")
--- javascript/jankenpon.js
+++ javascript/jankenpon.js
@@ -4,33 +4,52 @@ function startJankenpon(){
     let comp;
     let player;
     let lagi=true;
+    let inputplayer=false;
     let hasil;
     while(lagi==true){
         let random = Math.random()*jankenpon.length;
         comp = jankenpon[~~random]
-        player = prompt("Pilih salah satu kertas, gunting, batu dengan input text...");
-        if(player=="batu" && comp=="gunting"){
-            hasil = "Anda Menang";
+        for(inputplayer=false; inputplayer!=true;){
+            player = prompt("Pilih salah satu: \n- kertas \n- gunting \n- batu \nInputkan sesuai text di atas!");
+            if (player == "kertas" || player == "gunting" || player == "batu"){
+                inputplayer=true;
+            }
+            else if (player == null){
+                break;
+            }
+            else{
+                alert("Input tidak valid! \nLakukan input kembali!");
+                inputplayer=false;
+            }
         }
-        else if(player=="kertas" && comp=="batu"){
-            hasil = "Anda Menang";
-        }
-        else if(player=="gunting" && comp=="kertas"){
-            hasil = "Anda Menang";
-        }
-        else if(player=="batu" && comp=="batu"){
-            hasil = "Seri";
-        }
-        else if(player=="kertas" && comp=="kertas"){
-            hasil = "Seri";
-        }
-        else if(player=="gunting" && comp=="gunting"){
-            hasil = "Seri";
+        if(player == null){
+            alert("Selesai");
+            lagi=false;
         }
         else{
-            hasil = "Anda Kalah";
+            if(player=="batu" && comp=="gunting"){
+                hasil = "Anda Menang";
+            }
+            else if(player=="kertas" && comp=="batu"){
+                hasil = "Anda Menang";
+            }
+            else if(player=="gunting" && comp=="kertas"){
+                hasil = "Anda Menang";
+            }
+            else if(player=="batu" && comp=="batu"){
+                hasil = "Seri";
+            }
+            else if(player=="kertas" && comp=="kertas"){
+                hasil = "Seri";
+            }
+            else if(player=="gunting" && comp=="gunting"){
+                hasil = "Seri";
+            }
+            else{
+                hasil = "Anda Kalah";
+            }
+            alert("Review Pertandingan Jankenpon \nAnda = " + player + "\nComputer = " + comp + "\nHasil pertandingannya adalah  " + hasil);
+            lagi = confirm("Ingin main lagi?")
         }
-        alert("Anda = " + player + ", Computer = " + comp + ". Hasilnya " + hasil);
-        lagi = confirm("Ingin main lagi?")
     }
 }
